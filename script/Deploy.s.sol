58a198ecdcb219afab56431c14db3488b07c0139
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2025-06-09 13:05:58 +0700

    initial commit

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { Script, console } from "forge-std/Script.sol";
+
+contract Deploy is Script {
+    function setUp() public {}
+    function run() public {}
+}
---
+++ test/zkBring.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { Test, console } from "forge-std/Test.sol";
+
+contract zkBringTest is Test {
+    function setUp() public {}
+}
