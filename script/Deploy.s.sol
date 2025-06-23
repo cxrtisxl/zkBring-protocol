b333a243305953aa94447c07e255a26ed3242e2e
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2025-06-24 00:39:26 +0700

    Rename Deploy to DeployDev and integrate Semaphore setup
    Refactored the contract to DeployDev and added the Semaphore and zkBringRegistry initialization. Introduced TLSN Verifier address setup and console logging for contract addresses. This enables proper deployment and debugging for the new protocol dependencies.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,8 +2,24 @@
 pragma solidity ^0.8.23;
 
 import {Script, console} from "forge-std/Script.sol";
+import {ISemaphoreVerifier} from "semaphore-protocol/interfaces/ISemaphoreVerifier.sol";
+import {ISemaphore} from "semaphore-protocol/interfaces/ISemaphore.sol";
+import {SemaphoreVerifier} from "semaphore-protocol/base/SemaphoreVerifier.sol";
+import {Semaphore} from "semaphore-protocol/Semaphore.sol";
+import {zkBringRegistry} from "../src/registry/zkBringRegistry.sol";
 
-contract Deploy is Script {
+contract DeployDev is Script {
     function setUp() public {}
-    function run() public {}
+    function run() public {
+        // TLSN Verifier private key
+        // 0x7FA50A02193219D0625C2831908477D3568E5BEECA9AABE34381506A2431AFDE
+        address tlsnVerifierAddress = 0x3c50f7055D804b51e506Bc1EA7D082cB1548376C;
+        SemaphoreVerifier semaphoreVerifier = new SemaphoreVerifier();
+        Semaphore semaphore = new Semaphore(ISemaphoreVerifier(address(semaphoreVerifier)));
+        zkBringRegistry registry = new zkBringRegistry(ISemaphore(address(semaphore)), tlsnVerifierAddress);
+
+        console.log("Verifier:", address(semaphoreVerifier));
+        console.log("Semaphore:", address(semaphore));
+        console.log("Registry:", address(registry));
+    }
 }
